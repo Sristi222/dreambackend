6152b919bbbee8168c0da2c18c815e3c721c7dd0
Sristi Shrestha <sristishrestha80@gmail.com>
2025-03-18 21:06:30 +0545

    First committttt

--- server.js
+++ server.js
@@ -175,5 +175,5 @@ app.delete("/api/products/:id", verifyToken, async (req, res) => {
 });
 
 // **Start Server**
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
